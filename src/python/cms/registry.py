0018a54505ab40d60135cdb2571be6d0039afc7e
mmcentre <michael.bouzinier@kvitneset.com>
2021-12-16 22:04:58 -0500

    MAB            Updating copyright and license

--- src/python/cms/registry.py
+++ src/python/cms/registry.py
@@ -20,6 +20,25 @@ Admissions:     /data/incoming/rce/ci3_health_data/medicaid/cvd/1999_2012/desouz
 Sample user request: https://github.com/NSAPH/data_requests/tree/master/request_projects/feb2021_jenny_medicaid_resp
 '''
 
+#  Copyright (c) 2021. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Author: Michael A Bouzinier
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 import os
 from pathlib import Path
 
